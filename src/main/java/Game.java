432785b36be4b8de05e741938a6cb473a0dd20dc
beatrizremondes <115415537+beatrizremondes@users.noreply.github.com>
2023-10-02 17:46:17 +0100

    started 7 - keystroke

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,5 +1,6 @@
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyStroke;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
@@ -9,6 +10,9 @@ import java.io.IOException;
 
 public class Game {
     private final TerminalScreen screen;
+    private int x = 10;
+    private int y = 10;
+
 
     public Game(int  width, int height) throws IOException {
         TerminalSize terminalSize = new TerminalSize(width, height);
@@ -23,13 +27,18 @@ public class Game {
 
     private void draw() throws IOException{
         screen.clear();
-        screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')
-                [0]);
+        screen.setCharacter(x,y, TextCharacter.fromCharacter('X')[0]);
         screen.refresh();}
 
     public void run() throws IOException{
         draw();
+        KeyStroke key = screen.readInput();
+        processKey(key);
         }
+
+    private void processKey(KeyStroke key){
+        System.out.println(key);
+    }
 
 
 }

d8120ef8d040c413c71976f3b07883ce29899721
beatrizremondes <115415537+beatrizremondes@users.noreply.github.com>
2023-10-02 18:08:42 +0100

    started 7 - various keystrokes

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,6 +1,7 @@
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextCharacter;
 import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
@@ -14,7 +15,7 @@ public class Game {
     private int y = 10;
 
 
-    public Game(int  width, int height) throws IOException {
+    public Game(int width, int height) throws IOException {
         TerminalSize terminalSize = new TerminalSize(width, height);
         Terminal terminal = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize).createTerminal(); // terminal inteiro
 
@@ -25,19 +26,50 @@ public class Game {
 
     }
 
-    private void draw() throws IOException{
+    private void draw() throws IOException {
         screen.clear();
-        screen.setCharacter(x,y, TextCharacter.fromCharacter('X')[0]);
-        screen.refresh();}
+        screen.setCharacter(x, y, TextCharacter.fromCharacter('X')[0]);
+        screen.refresh();
+    }
 
-    public void run() throws IOException{
-        draw();
-        KeyStroke key = screen.readInput();
-        processKey(key);
+    public void run() throws IOException {
+        while (true) {
+            draw();
+            KeyStroke key = screen.readInput();
+
+            // Process the key stroke if one is available
+            if (key != null) {
+                processKey(key);
+            }
+        }
+    }
+
+    public void processKey(KeyStroke keyStroke)  {
+        KeyType keyType = keyStroke.getKeyType();
+
+        switch (keyType) {
+            case ArrowUp:
+                // If the ArrowUp key is pressed, decrement y by 1
+                y--;
+                break;
+            case ArrowRight:
+                // If the ArrowRight key is pressed, increment x by 1
+                x++;
+                break;
+            case ArrowDown:
+                // If the ArrowDown key is pressed, increment y by 1
+                y++;
+                break;
+            case ArrowLeft:
+                // If the ArrowLeft key is pressed, decrement x by 1
+                x--;
+                break;
+            default:
+                // Handle other key types or do nothing
+                break;
         }
 
-    private void processKey(KeyStroke key){
-        System.out.println(key);
+        // You can add additional logic here as needed for your game
     }
 
 
